9f07d916257c81803173e006d8191c1db5c90dfc
ruslan.palagin <ruslan.palagin@masterofcode.com>
2013-10-30 19:36:33 +0200

    category priority

---
+++ app/DoctrineMigrations/Version20131030184937.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20131030184937 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+
+        $this->addSql("ALTER TABLE Category ADD priority INT NOT NULL");
+        $this->addSql("UPDATE Category c SET c.priority = c.id");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE Category DROP priority");
+    }
+}
